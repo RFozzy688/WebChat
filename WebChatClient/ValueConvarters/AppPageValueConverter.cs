6bc150909398499c3bd29948e9b4702cfa83693e
RFozzy688 <pokerfozzy@gmail.com>
2023-12-30 10:54:38 +0200

    создание конвертера значений для страницы входа

---
+++ WebChatClient/ValueConvarters/AppPageValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebChatClient
+{
+    /// <summary>
+    /// Преобразует ApplPage в фактическое представление/страницу.
+    /// </summary>
+    public class AppPageValueConverter : BaseValueConverter<AppPageValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((AppPage)value) 
+            {
+                // поиск соответствующей стрвницы
+                case AppPage.Login:
+                    return new LoginPage();
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
