56072ef6cc0661e49456f6879e6eac3ee03eb687
RFozzy688 <pokerfozzy@gmail.com>
2024-01-06 18:23:40 +0200

    Пузырьки сообщений в чате

---
+++ WebChatClient/Controls/ChatMessage/ChatMessageListItemControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WebChatClient
+{
+    /// <summary>
+    /// Логика взаимодействия для ChatMessageListItemControl.xaml
+    /// </summary>
+    public partial class ChatMessageListItemControl : UserControl
+    {
+        public ChatMessageListItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
