f936770bfe56bf3d6e430d5243f79ce46d24ee9c
RFozzy688 <pokerfozzy@gmail.com>
2024-01-07 06:39:01 +0200

    при загрузки приложения устанавливается фокус на поле email

---
+++ WebChatClient/AttachedProperties/TextAttachedProperties.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WebChatClient
+{
+    /// <summary>
+    /// Фокусирует (фокус клавиатуры) этот элемент при загрузке
+    /// </summary>
+    public class IsFocusedProperty : BaseAttachedProperty<IsFocusedProperty, bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            // Если у нас нет control, вернёмся
+            if (!(sender is Control control))
+                return;
+
+            // Сфокусируйте этот элемент управления после загрузки
+            control.Loaded += (s, se) => control.Focus();
+        }
+    }
+}
