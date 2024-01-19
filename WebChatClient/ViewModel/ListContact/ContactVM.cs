442a1ec56f3169124b457bb4966e73aaecca3eb6
RFozzy688 <pokerfozzy@gmail.com>
2024-01-19 02:19:58 +0200

    загрузка контактов в список контактов, анимация при выборе контакта

---
+++ WebChatClient/ViewModel/ListContact/ContactVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WebChatClient
+{
+    /// <summary>
+    /// модель представления контактов
+    /// </summary>
+    public class ContactVM : BaseViewModel
+    {
+        // Отображаемое имя в списке чатов
+        public string Name { get; set; }
+
+        // Последнее сообщение из этого чата
+        public string Message { get; set; }
+
+        // Инициалы, которые будут отображаться в качестве фона изображения профиля.
+        public string Initials { get; set; }
+
+        // Значения RGB (в шестнадцатеричном формате) для цвета фона изображения профиля.
+        public string ProfilePictureRGB { get; set; }
+
+        // Верно, если в этом чате есть непрочитанные сообщения.
+        public bool NewContentAvailable { get; set; }
+
+        // Истинно, если этот элемент выбран в данный момент
+        public bool IsSelected { get; set; }
+
+        public ContactVM()
+        {
+        }
+
+    }
+}
