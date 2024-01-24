7b06fec6067ab156e68e3822b91e893b45b501be
RFozzy688 <pokerfozzy@gmail.com>
2024-01-24 18:52:36 +0200

    разработка сервера

---
+++ WebChatServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WebChatServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
